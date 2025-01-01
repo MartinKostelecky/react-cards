6bc3d12e458d85e33a61e8974a978d363fc45c60
MartinKostelecky <kosteleckymartin@gmail.com>
2025-01-01 22:17:42 +0100

    Props added to customize each card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Card from "./Card.jsx"
+import pizzaPic from './assets/pizza.jpg'
+import docPic from './assets/doctor.jpg'
+import taxiPic from './assets/taxi.jpg'
 
 function App() {
 
   return (
     <>
+      <Card number={1} title="Pizza" phoneNumber="111 222 333" isOnline={true} image={pizzaPic}/>
+      <Card number={2} title="Doctor" phoneNumber="111 111 111" isOnline={false} image={docPic}/>
+      <Card number={3} title="Taxi" phoneNumber="123 456 789" isOnline={true} image={taxiPic}/>
       <Card />
-      
     </>
   );
 
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,15 +1,33 @@
+import PropTypes, { number } from 'prop-types'
 
-import pizzaPic from './assets/pizza.jpg'
+function Card(props) {
+    const statusStyle = { color: props.isOnline ? 'green' : 'red', };
 
-function Card() {
     return(
         <div className="card">
-            <h1 className="card-title">#1</h1>
-            <img className="card-image" src={pizzaPic} alt="pizza picture" />
-            <h2 className="card-title">Pizza</h2>
-            <p className="card-text">Tel: 111 222 333</p>
+            <h1 className="card-title">{props.number}</h1>
+            <img className="card-image" src={props.image} alt={props.title} />
+            <h2 className="card-title">{props.title}</h2>
+            <p className="card-text">Tel: {props.phoneNumber}</p>
+            <p className="card-text" style={statusStyle}>{props.isOnline ? "Online" : "Offline"}</p>
         </div>
     ); 
 }
+
+//good practice to add PropTypes when working with props
+Card.propTypes = {
+    number: PropTypes.number,
+    title: PropTypes.string,
+    phoneNumber: PropTypes.string,
+    isOnline: PropTypes.bool
+}
+
+Card.defaultProps = {
+    number: 0,
+    title: "Not added",
+    phoneNumber: "Not added",
+    isOnline: false
+}
+
 
 export default Card
